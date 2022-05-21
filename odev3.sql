66c945e51612c8220e2ee6d58a356472ccd8c68a
rabiakocabey <81436066+rabiakocabey@users.noreply.github.com>
2022-05-22 02:55:07 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,24 @@
+--1.soru 
+
+SELECT country 
+FROM country 
+WHERE country LIKE 'A%a';
+
+--2.soru 
+
+SELECT country 
+FROM country 
+WHERE country LIKE '_____%n';
+
+--3.soru 
+
+SELECT title
+FROM film
+WHERE title ILIKE '%T%T%T%';
+
+--4.soru 
+
+
+SELECT * 
+FROM film
+WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
